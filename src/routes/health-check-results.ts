cc6372e46b16c00e3d3d488e98f4e7a4cbd7c2c0
Christoffer Artmann <artgaard@gmail.com>
2025-11-07 21:17:13 +0100

    feat: Update the health check endpoint.

--- src/routes/health-check-results.ts
+++ src/routes/health-check-results.ts
@@ -3,15 +3,26 @@ import { log } from 'tiny-typescript-logger'
 
 import { HealthCheckResult } from '../models/health-check-result'
 import { serializeHealthCheckResult } from '../serializers'
+import { HealthCheck } from '../models/health-check'
 
 export const healthCheckResultsRouter = new Hono()
 
 healthCheckResultsRouter.get('/', async (context) => {
-  const healthCheckResults = await HealthCheckResult.all()
+  const checks = await HealthCheck.all()
+  const checkIds = checks.map((c) => c.id)
+ 
+  const healthCheckResults = await HealthCheckResult.whereIn(
+    'healthCheckid',
+    checkIds
+  )
+    .orderBy('createdAt', 'desc')
+    .get()
 
   log.info(`Retrieved ${healthCheckResults.length} health check results.`)
 
   return context.json({
-    healthCheckResults: healthCheckResults.map(hc => serializeHealthCheckResult(hc))
+    healthCheckResults: healthCheckResults.map((hc) =>
+      serializeHealthCheckResult(hc)
+    )
   })
 })
