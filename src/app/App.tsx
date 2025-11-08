049109f135cbf57323aad3b19c47d448ff07812c
Christoffer Artmann <artgaard@gmail.com>
2025-11-08 22:37:21 +0100

    feat: Add React Query.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,23 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { StrictMode } from 'react'
 import { Route, Routes } from 'react-router-dom'
 
 import './index.css'
+import { OverviewPage } from './pages/OverviewPage'
+
+const queryClient = new QueryClient()
 
 export function App() {
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={<div>Foo</div>}
-      />
-    </Routes>
+    <StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <Routes>
+          <Route
+            path="/"
+            element={<OverviewPage />}
+          />
+        </Routes>
+      </QueryClientProvider>
+    </StrictMode>
   )
 }
---
+++ src/app/pages/OverviewPage.tsx
@@ -0,0 +1,5 @@
+import { ReactElement } from 'react'
+
+export function OverviewPage(): ReactElement {
+  return <div>Overview Page</div>
+}
